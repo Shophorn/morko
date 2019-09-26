72e140d0222c721a123f42446192589db54af9c3
unknown <shophorn@protonmail.com>
2019-09-26 12:18:25 +0300

    Start lol

---
+++ MorkoUnityProject/Assets/Scripts/Network/Client.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Morko.Network
+{
+	[System.Flags]
+	enum AvatarFlags
+	{
+		Attack = 1
+	}
+
+	public class AvatarPackage
+	{
+		int id;
+		Vector3 position;
+		Quaternion rotation;
+		Vector3 velocity;
+		AvatarFlags flags;
+	}
+
+
+	public class Client
+	{
+
+	}
+}
