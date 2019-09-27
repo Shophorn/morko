dcbc033a17287d13ff6679c417e6432e7c870abc
SAMPROO <sampo.tuisku@gmail.com>
2019-09-27 12:45:48 +0300

    PlayerSettings
    Using scriptableObject
    Change player to morko function

---
+++ MorkoUnityProject/Assets/Scripts/PlayerSettings.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerSettings", menuName = "ScriptableObjects/PlayerSettings")]
+public class PlayerSettings : ScriptableObject
+{   
+    [Header("Character normal form")]
+    [Range(1f, 10f)] public float movementSpeed = 5f;
+    [Header("Set multiplier to 1 for no effect")]
+    [Range(0.1f, 1f)] public float sneakMultiplier = 0.5f;
+    [Range(1f, 2f)] public float runMultiplier = 1.5f;
+
+    [Header("Mörkö")]
+    [Space(10)]
+    
+    [Range(1f, 10f)] public float morkoMovementSpeed = 5f;
+    [Header("Set multiplier to 1 for no effect")]
+    [Range(0.1f, 1f)] public float morkoSneakMultiplier = 0.3f;
+    [Range(1f, 2f)] public float morkoRunMultiplier = 1.2f;
+}
