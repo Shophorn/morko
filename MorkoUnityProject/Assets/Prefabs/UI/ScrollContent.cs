104e8a48955b42574348467e181006176664d1b5
Joonas-Aaltonen <32293674+Joonas-Aaltonen@users.noreply.github.com>
2019-10-08 00:32:43 +0300

    Add files via upload
    Some UI stuff
    - Partial support for a controller
    - Looping scrolling list for selecting levels

---
+++ MorkoUnityProject/Assets/Prefabs/UI/ScrollContent.cs
@@ -0,0 +1,188 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Morko
+{
+	public class ScrollContent : MonoBehaviour
+	{
+		#region Public Properties
+
+		public ListItem listElement;
+		public ListItem[] listElements;
+		private GameObject listGrid;
+
+		public InfiniteScroll levelSelectionList;
+
+		public ListItem currentItem;
+
+		public Sprite[] levelImages;
+
+		public GameObject gridCenter;
+
+		private int numberOfScenes;
+		private string[] levelNames;
+
+
+		/// <summary>
+		/// How far apart each item is in the scroll view.
+		/// </summary>
+		public float ItemSpacing { get { return itemSpacing; } }
+
+		/// <summary>
+		/// How much the items are indented from left and right of the scroll view.
+		/// </summary>
+		public float HorizontalMargin { get { return horizontalMargin; } }
+
+		/// <summary>
+		/// How much the items are indented from top and bottom of the scroll view.
+		/// </summary>
+		public float VerticalMargin { get { return verticalMargin; } }
+
+		/// <summary>
+		/// Is the scroll view oriented horizontally?
+		/// </summary>
+		public bool Horizontal { get { return horizontal; } }
+
+		/// <summary>
+		/// Is the scroll view oriented vertically?
+		/// </summary>
+		public bool Vertical { get { return vertical; } }
+
+		/// <summary>
+		/// The width of the scroll content.
+		/// </summary>
+		public float Width { get { return width; } }
+
+		/// <summary>
+		/// The height of the scroll content.
+		/// </summary>
+		public float Height { get { return height; } }
+
+		#endregion
+
+		#region Private Members
+
+		/// <summary>
+		/// The RectTransform component of the scroll content.
+		/// </summary>
+		private RectTransform rectTransform;
+
+		/// <summary>
+		/// The width and height of the parent.
+		/// </summary>
+		private float width, height;
+
+
+		/// <summary>
+		/// How far apart each item is in the scroll view.
+		/// </summary>
+		[SerializeField]
+		private float itemSpacing;
+
+		/// <summary>
+		/// How much the items are indented from the top/bottom and left/right of the scroll view.
+		/// </summary>
+		[SerializeField]
+		private float horizontalMargin, verticalMargin;
+
+		/// <summary>
+		/// Is the scroll view oriented horizontall or vertically?
+		/// </summary>
+		[SerializeField]
+		private bool horizontal, vertical;
+
+		#endregion
+
+		private void Start()
+		{
+			listGrid = GameObject.Find("ScrollContent");
+			CreateList();
+
+			rectTransform = GetComponent<RectTransform>();
+
+			// Subtract the margin from both sides.
+			width = rectTransform.rect.width - (2 * horizontalMargin);
+
+			// Subtract the margin from the top and bottom.
+			height = rectTransform.rect.height - (2 * verticalMargin);
+
+			horizontal = !vertical;
+			if (vertical)
+				InitializeContentVertical();
+			else
+				InitializeContentHorizontal();
+
+			levelSelectionList.listElements = listElements;
+		}
+
+		private void CreateList()
+		{
+			numberOfScenes = SceneManager.sceneCountInBuildSettings;
+			listElements = new ListItem[numberOfScenes - 1];
+			levelNames = new string[numberOfScenes - 1];
+
+			for (int i = 0; i < numberOfScenes - 1; i++)
+			{
+				string path = SceneUtility.GetScenePathByBuildIndex(i + 1);
+				string levelName = System.IO.Path.GetFileNameWithoutExtension(path);
+				levelNames[i] = levelName;
+			}
+
+			if (listGrid != null)
+			{
+				for (int i = 0; i < numberOfScenes - 1; i++)
+				{
+					listElements[i] = Instantiate(listElement, new Vector3(listGrid.transform.position.x, listGrid.transform.position.y, 0), Quaternion.identity);
+					listElements[i].id = i;
+					listElements[i].transform.Find("Level Name").GetComponent<Text>().text = levelNames[i];
+					listElements[i].transform.Find("Level Image").GetComponent<Image>().sprite = levelImages[i];
+					listElements[i].transform.parent = listGrid.transform;
+				}
+				currentItem = listElements[0];
+
+				for (int i = 0; i < numberOfScenes - 1; i++)
+				{
+					if (listElements[i].id != listElements[listElements.Length - 1].id)
+						listElements[i].nextItem = listElements[i + 1];
+					else
+						listElements[i].nextItem = listElements[0];
+					if (listElements[i].id != listElements[0].id)
+						listElements[i].lastItem = listElements[i - 1];
+					else
+						listElements[i].lastItem = listElements[listElements.Length - 1];
+				}
+			}
+		}
+
+		/// <summary>
+		/// Initializes the scroll content if the scroll view is oriented horizontally.
+		/// </summary>
+		private void InitializeContentHorizontal()
+		{
+			float originX = 0 - (width * 0.25f);
+			float posOffset = width * 0.25f;
+			for (int i = 0; i < numberOfScenes - 1; i++)
+			{
+				Vector3 childPos = listElements[i].transform.localPosition;
+				childPos.x = originX + posOffset + i * (width + itemSpacing);
+				listElements[i].transform.localPosition = childPos;
+			}
+		}
+
+		/// <summary>
+		/// Initializes the scroll content if the scroll view is oriented vertically.
+		/// </summary>
+		private void InitializeContentVertical()
+		{
+			float originY = 0 - (height * 0.5f);
+			float posOffset = height * 0.5f;
+			for (int i = 0; i < numberOfScenes - 1; i++)
+			{
+				Vector2 childPos = listElements[i].transform.localPosition;
+				childPos.y = originY + posOffset + i * (height + itemSpacing);
+				listElements[i].transform.localPosition = childPos;
+			}
+		}
+	}
+}
---
+++ MorkoUnityProject/Assets/Prefabs/UI/WindowLoadingSystem.cs
@@ -0,0 +1,268 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Morko
+{
+    public class WindowLoadingSystem : MonoBehaviour
+    {
+        public GameObject mainMenu;
+        public GameObject menuWindow;
+        public GameObject hostWindow;
+        public GameObject pauseWindow;
+		public GameObject loadWindow;
+
+		public GameObject[] menuWindowItems;
+		public GameObject[] hostWindowItems;
+
+		public ScrollContent scrollContent;
+
+        //private int numberOfScenes;
+        //private string[] sceneNames;
+
+        private Dropdown dropdown = null;
+
+		private int currentActiveSelection;
+		private bool isAxisInUse;
+		//private bool dropdownShown;
+
+        void Start()
+        {
+            //numberOfScenes = SceneManager.sceneCountInBuildSettings;
+            //sceneNames = new string[numberOfScenes];
+            //for(int i = 0; i < numberOfScenes; i++)
+            //{
+            //    string path = SceneUtility.GetScenePathByBuildIndex(i);
+            //    string sceneName = System.IO.Path.GetFileNameWithoutExtension(path);
+            //    sceneNames[i] = sceneName;
+            //}
+            mainMenu.SetActive(true);
+            menuWindow.SetActive(true);
+            hostWindow.SetActive(false);
+            pauseWindow.SetActive(false);
+			loadWindow.SetActive(false);
+
+			currentActiveSelection = 1;
+			ChangeSelection(currentActiveSelection);
+        }
+
+        void Update()
+        {
+			if (Input.GetKeyUp(KeyCode.Escape) || Input.GetButtonUp("Cancel"))
+            {
+                TogglePauseWindow();
+            }
+			if(menuWindow.activeInHierarchy)
+				MoveInMenuWindow();
+			//if (hostWindow.activeInHierarchy && !dropdownShown)
+				//MoveInHostWindow();
+
+			//if (!dropdownShown && Input.GetButtonDown("Cancel"))
+				//BackToMainMenu();
+		}
+
+		public void ToggleMainMenu()
+        {
+            mainMenu.SetActive(!mainMenu.activeInHierarchy);
+        }
+
+        public void ToggleHostWindow()
+        {
+            hostWindow.SetActive(!hostWindow.activeInHierarchy);
+        }
+
+        public void ToggleMenuWindow()
+        {
+            menuWindow.SetActive(!menuWindow.activeInHierarchy);
+        }
+
+        public void TogglePauseWindow()
+        {
+            if(!hostWindow.activeInHierarchy && !mainMenu.activeInHierarchy)
+                pauseWindow.SetActive(!pauseWindow.activeInHierarchy);
+        }
+
+		public void ToggleLoadWindow()
+		{
+			loadWindow.SetActive(!loadWindow.activeInHierarchy);
+		}
+
+        public void BackToMainMenu()
+        {
+			currentActiveSelection = 1;
+			ChangeSelection(currentActiveSelection);
+            if(hostWindow.activeInHierarchy == false)
+                SceneManager.UnloadSceneAsync(scrollContent.currentItem.id+1);
+            mainMenu.SetActive(true);
+            menuWindow.SetActive(true);
+            hostWindow.SetActive(false);
+            pauseWindow.SetActive(false);
+        }
+
+        //public int GetSceneIndexByName(string sceneName)
+        //{
+        //    for(int i = 1; i < sceneNames.Length; i++)
+        //    {
+        //        if(sceneName == sceneNames[i])
+        //            return i;
+        //    }
+        //    return -1;
+        //}
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+
+        public void HostGame()
+        {
+            ToggleHostWindow();
+            ToggleMenuWindow();
+			currentActiveSelection = 3;
+			ChangeSelection(currentActiveSelection);
+            //dropdown = GameObject.FindObjectOfType<Dropdown>();
+            //dropdown.ClearOptions();
+   //         for(int i = 1; i < sceneNames.Length; i++)
+   //         {
+			//	Dropdown.OptionData data = new Dropdown.OptionData(sceneNames[i]);
+			//	dropdown.options.Add(data);
+			//}
+        }
+
+        public void StartGame()
+        {
+            // Dropdown.value is used to identify scene from Build Settings: Scenes in Build
+            // scene 0 on Menu
+            StartCoroutine(LoadScene(scrollContent.currentItem.id + 1));
+            ToggleMainMenu();
+            ToggleHostWindow();
+        }
+
+        IEnumerator LoadScene(int sceneIndex) // This async operation initiates character instantiation after the correct scene has been loaded and set as active
+        {
+			ToggleLoadWindow();
+            AsyncOperation load = SceneManager.LoadSceneAsync((sceneIndex),LoadSceneMode.Additive);
+            while (!load.isDone)
+            {
+                if (load.progress >= 0.95f)
+                {
+
+                }
+                yield return null;
+            }
+            // After scene has been loaded
+            SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(sceneIndex));
+            ProtoGameCreator.Instance.StartScene();
+			ToggleLoadWindow();
+        }
+
+        public void ExitCurrentGame()
+        {
+            //SceneManager.UnloadSceneAsync(dropdown.value+1);
+            SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(0));
+            BackToMainMenu();
+        }
+
+		public void ChangeSelection(int currentSelection)
+		{
+			switch (currentSelection)
+			{
+				case 1:
+					Button hostButton = menuWindow.transform.Find("Host Button").GetComponent<Button>();
+					hostButton.Select();
+					break;
+				case 2:
+					Button joinButton = menuWindow.transform.Find("Join Button").GetComponent<Button>();
+					joinButton.Select();
+					break;
+				case 3:
+					Button startButton = hostWindow.transform.Find("Start Game Button").GetComponent<Button>();
+					startButton.Select();
+					break;
+				case 4:
+					Button quitButton = menuWindow.transform.Find("Quit Button").GetComponent<Button>();
+					quitButton.Select();
+					break;
+				case 5:
+					Button backToMenuButton = hostWindow.transform.Find("Back To Menu Button").GetComponent<Button>();
+					backToMenuButton.Select();
+					break;
+				case 6:
+					InputField nameInput = hostWindow.transform.Find("Host Name Field").GetComponent<InputField>();
+					nameInput.Select();
+					break;
+			}
+		}
+
+
+		private void MoveInMenuWindow()
+		{
+			if (Input.GetAxisRaw("Vertical") == -1)
+			{
+				if (!isAxisInUse)
+				{
+
+					currentActiveSelection++;
+					if (currentActiveSelection == 5)
+						currentActiveSelection = 1;
+					if (currentActiveSelection == 3)
+						currentActiveSelection = 4;
+					ChangeSelection(currentActiveSelection);
+					isAxisInUse = true;
+				}
+			}
+			if (Input.GetAxisRaw("Vertical") == 1)
+			{
+				if (!isAxisInUse)
+				{
+					currentActiveSelection--;
+					if (currentActiveSelection == 0)
+						currentActiveSelection = 4;
+					if (currentActiveSelection == 3)
+						currentActiveSelection = 2;
+					ChangeSelection(currentActiveSelection);
+					isAxisInUse = true;
+				}
+			}
+			if (Input.GetAxisRaw("Vertical") == 0)
+				isAxisInUse = false;
+		}
+
+		private void MoveInHostWindow()
+		{
+			if (Input.GetAxisRaw("Vertical") == -1)
+			{
+				if (!isAxisInUse)
+				{
+					currentActiveSelection++;
+					if (currentActiveSelection == 4)
+						currentActiveSelection = 5;
+					if (currentActiveSelection > 6)
+						currentActiveSelection = 3;
+					if (currentActiveSelection < 3)
+						currentActiveSelection = 6;
+					ChangeSelection(currentActiveSelection);
+					isAxisInUse = true;
+				}
+			}
+			if (Input.GetAxisRaw("Vertical") == 1)
+			{
+				if (!isAxisInUse)
+				{
+					currentActiveSelection--;
+					if (currentActiveSelection == 4)
+						currentActiveSelection = 3;
+					if (currentActiveSelection > 6)
+						currentActiveSelection = 3;
+					if (currentActiveSelection < 3)
+						currentActiveSelection = 6;
+					ChangeSelection(currentActiveSelection);
+					isAxisInUse = true;
+				}
+			}
+			if (Input.GetAxisRaw("Vertical") == 0)
+				isAxisInUse = false;
+		}
+    }
+}
