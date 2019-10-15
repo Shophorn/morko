f615b392f293f71c7c1f047b30d6b718a3d2553d
unknown <shophorn@protonmail.com>
2019-10-15 10:40:48 +0300

    Network 2

--- Network/Constants.cs
+++ Network/Constants.cs
@@ -12,7 +12,9 @@ namespace Morko.Network
 	public static class Constants
 	{
 		public const int broadcastPort = 11000;
-		public static readonly IPAddress broadcastAddress = IPAddress.Broadcast; // 255.255.255.255
+		// public static readonly IPAddress broadcastAddress = IPAddress.Broadcast; // 255.255.255.255
+		// Note(Leo): Only for testing
+		public static readonly IPAddress broadcastAddress = IPAddress.Parse("192.168.43.255");
 
 		// Note(Leo): https://en.wikipedia.org/wiki/Multicast_address
 		public const int multicastPort = 21001;
