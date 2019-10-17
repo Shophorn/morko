b8288d3f8fb9f5417075a84a7bf7012c43c4d31a
Iiro <pelttarii@gmail.com>
2019-10-17 23:50:47 +0300

    Testing

---
+++ MorkoUnityProject/Assets/Scripts/Vision/ProtoMapScaler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtoMapScaler : MonoBehaviour
+{
+    public Transform[] pavement;
+    public float currentScale = 1f;
+    public float baseScale = 3f;
+    private void OnValidate()
+    {
+        foreach(var pave in pavement)
+        {
+            pave.localScale = new Vector3(baseScale * currentScale, pave.localScale.y, pave.localScale.z);
+            
+        }
+    }
+}
