8a3b2a32dba2418440e3d5e89e2ca947e05b521b
SAMPROO <sampo.tuisku@gmail.com>
2019-10-17 16:31:48 +0300

    PR Code cleanup

--- MorkoUnityProject/Assets/Scripts/LocalController.cs
+++ MorkoUnityProject/Assets/Scripts/LocalController.cs
@@ -13,6 +13,7 @@ namespace Morko
 		private Camera camera;
 		private Vector3 lastMousePosition = Input.mousePosition;
 		private LayerMask groundMask = 1 << 9;
+		private const float joystickMaxThreshold = 0.8f;
 		
 		// mouseRotate = True == rotate with mouse
 		// mouseRotate = False == rotate with joystick
@@ -77,17 +78,16 @@ namespace Morko
 			
 			moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
 			
-			moveDirection = moveDirection.normalized;
-
 			float joystickRotateX = Input.GetAxis("RotateX");
 			float joystickRotateY = Input.GetAxis("RotateY");
 
 			Vector3 currentMousePosition = Input.mousePosition;
 			Vector3 mouseDelta = currentMousePosition - lastMousePosition;
 			
+			bool mouseMoved = Mathf.Abs(mouseDelta.x) > 0 || Mathf.Abs(mouseDelta.y) > 0;
+			
 			// Rotate
-			// Mouse moved, use mouse
-			if (Mathf.Abs(mouseDelta.x) > 0 || Mathf.Abs(mouseDelta.y) > 0)
+			if (mouseMoved)
 			{
 				mouseRotate = true;
 				lastMousePosition = currentMousePosition;
@@ -98,8 +98,12 @@ namespace Morko
 				Vector3 lookDirectionLevel = new Vector3(lookDirection.x, character.transform.position.y, lookDirection.z);
 				character.transform.rotation = Quaternion.LookRotation(lookDirectionLevel);
 			}
-			// If joystick is used for rotation, use controller
-			if (Mathf.Abs(joystickRotateX) > 0f || Mathf.Abs(joystickRotateY) > 0f )
+			
+			bool joystickForRotation = Mathf.Abs(joystickRotateX) > 0f || Mathf.Abs(joystickRotateY) > 0f;
+			bool onlyLeftJoystickUsed = moveDirection != Vector3.zero && joystickRotateX == 0 && joystickRotateY == 0 && mouseDelta.x == 0 && mouseDelta.y == 0 && mouseRotate == false;
+			bool mouseForRotation = joystickRotateX == 0 && joystickRotateY == 0 && mouseDelta.x == 0 && mouseDelta.y == 0 && mouseRotate == true;
+			
+			if (joystickForRotation)
 			{
 				mouseRotate = false;
 				Vector3 lookDirectionJoystick = new Vector3(Input.GetAxis("RotateX"), 0f, Input.GetAxis("RotateY"));
@@ -107,43 +111,38 @@ namespace Morko
        
 				character.transform.rotation = Quaternion.RotateTowards(lookRotation, character.transform.rotation, Time.deltaTime);
 			}
-			// Controller being used, right joystick not being used, look towards player forward
-			if (moveDirection != Vector3.zero && joystickRotateX == 0 && joystickRotateY == 0 && mouseDelta.x == 0 && mouseDelta.y == 0 && mouseRotate == false)
-			{
+			if (onlyLeftJoystickUsed)
 				character.transform.rotation = Quaternion.LookRotation(moveDirection);
-			}
-			// Mouse being used, keep old rotation
-			if (joystickRotateX == 0 && joystickRotateY == 0 && mouseDelta.x == 0 && mouseDelta.y == 0 && mouseRotate == true)
-			{
+			if (mouseForRotation)
 				character.transform.rotation = package.rotation;
-			}
 			
-			// Hardcoded min magnitude value where joystick is maxed out
-			bool joystickMaxed = moveDirection.magnitude >= 0.8;
+			bool joystickMaxed = moveDirection.magnitude >= joystickMaxThreshold;
 			
-			// Sneak
-			if (moveDirection != Vector3.zero && joystickMaxed == false && currentMovementSpeed <= sneakSpeed)
+			bool sneak = moveDirection != Vector3.zero && joystickMaxed == false && currentMovementSpeed <= sneakSpeed;
+			bool accelerateWalk = joystickMaxed == true && currentMovementSpeed < walkSpeed;
+			bool decelerateWalk = (moveDirection == Vector3.zero || joystickMaxed == false) && currentMovementSpeed <= walkSpeed && currentMovementSpeed > 0f;
+			bool maxWalkSpeed = joystickMaxed == true && currentMovementSpeed >= walkSpeed && ran == false;
+			bool accelerateRun = (Input.GetButton("Sprint") || Input.GetKey(KeyCode.LeftShift) == true) && joystickMaxed && currentMovementSpeed >= walkSpeed;
+			bool decelerateRun = Input.GetButton("Sprint") == false && Input.GetKey(KeyCode.LeftShift) == false && currentMovementSpeed > walkSpeed && ran;
+			bool maxRunSpeed = (Input.GetButton("Sprint") || Input.GetKey(KeyCode.LeftShift) == true) && joystickMaxed && currentMovementSpeed >= runSpeed;
+			
+			if (sneak)
 				currentMovementSpeed = sneakSpeed;
-			// Accelerate walk
-			else if (joystickMaxed == true && currentMovementSpeed < walkSpeed)
+			else if (accelerateWalk)
 				currentMovementSpeed += accelerationWalk * Time.deltaTime;
-			// Decelerate walk
-			else if ((moveDirection == Vector3.zero || joystickMaxed == false) && currentMovementSpeed <= walkSpeed && currentMovementSpeed > 0f)
+			else if (decelerateWalk)
 				currentMovementSpeed -= decelerationWalk * Time.deltaTime;
-			// Maximum running speed
-			else if ((Input.GetButton("Sprint") || Input.GetKey(KeyCode.LeftShift) == true) && joystickMaxed && currentMovementSpeed >= runSpeed)
+			else if (maxRunSpeed)
 			{
 				ran = true;
 				currentMovementSpeed = runSpeed;
 			}
-			// Accelerate run
-			else if ((Input.GetButton("Sprint") || Input.GetKey(KeyCode.LeftShift) == true) && joystickMaxed && currentMovementSpeed >= walkSpeed)
+			else if (accelerateRun)
 			{
 				ran = true;
 				currentMovementSpeed += accelerationRun * Time.deltaTime;
 			}
-			// Decelerate run
-			else if (Input.GetButton("Sprint") == false && Input.GetKey(KeyCode.LeftShift) == false && currentMovementSpeed > walkSpeed && ran)
+			else if (decelerateRun)
 			{
 				currentMovementSpeed -= decelerationRun * Time.deltaTime;
 				if (currentMovementSpeed <= walkSpeed)
@@ -152,10 +151,8 @@ namespace Morko
 					currentMovementSpeed = walkSpeed;
 				}
 			}
-			// Maximum walking speed
-			else if (joystickMaxed == true && currentMovementSpeed >= walkSpeed && ran == false)
+			else if (maxWalkSpeed)
 				currentMovementSpeed = walkSpeed;
-			// Still
 			else
 				currentMovementSpeed = 0f;
 			
@@ -164,17 +161,20 @@ namespace Morko
 			if (moveDirection != Vector3.zero)
 				oldDirection = moveDirection;
 			
-			moveDirection = currentMovementSpeed > 0 ? oldDirection.normalized : Vector3.zero;
+			moveDirection = currentMovementSpeed > 0 ? oldDirection : Vector3.zero;
 			
 			// Dot product between character facing direction and character moving direction
 			// Parallel == 1
 			// Perpendicular == 0
 			// Opposite == -1
-			float moveLookDotProduct = Vector3.Dot(moveDirection, character.transform.forward);
+			float moveLookDotProduct = Vector3.Dot(moveDirection.normalized, character.transform.forward);
 			
 			float decrease = 1f;
-			// Apply sideways multiplier lerp
-			if (moveLookDotProduct >= 0)
+			
+			bool movingSideways = moveLookDotProduct >= 0 && moveLookDotProduct < 1f;
+			bool movingBackwards = moveLookDotProduct < 0 && moveLookDotProduct >= -1f;
+			
+			if (movingSideways)
 			{
 				// Walk side multiplier
 				if (currentMovementSpeed <= walkSpeed)
@@ -183,8 +183,7 @@ namespace Morko
 				else
 					decrease = Mathf.Lerp(sideRunMultiplier, 1f, moveLookDotProduct);
 			}
-			// Apply backwards multiplier lerp
-			else
+			else if (movingBackwards)
 			{
 				// Walk backwards multiplier
 				if (currentMovementSpeed <= walkSpeed)
@@ -193,6 +192,8 @@ namespace Morko
 				else
 					decrease = Mathf.Lerp(sideRunMultiplier, backwardRunMultiplier, Mathf.Abs(moveLookDotProduct));
 			}
+			else
+				decrease = 1f;
 			
 			// TODO (Sampo): When turning backwards and then back forwards, acceleration should be applied
 			// Currently it goes straight back to currenMovementSpeed
