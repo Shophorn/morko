b25df6c74c3d8e2d37874196ad18b79ed7b134fb
Iiro <pelttarii@gmail.com>
2019-12-05 01:02:03 +0200

    Menu Audio

--- MorkoUnityProject/Assets/Code/AudioController.cs
+++ MorkoUnityProject/Assets/Code/AudioController.cs
@@ -6,12 +6,16 @@ using UnityEngine.Audio;
 public class AudioController : MonoBehaviour, IAudioUIControllable
 {
     public AudioMixer mixer;
+
+    [Header("Mixer Channels")]
     public AudioMixerGroup master;
     public AudioMixerGroup game;
     public AudioMixerGroup ui;
     public AudioMixerGroup music;
-
-
+    [Header("UI Sounds")]
+    public AudioClip onSelect;
+    public AudioClip[] onClick;
+    public AudioSource audioSRC;
     /* Note(Leo): Implementing these explicitly we get a nice
     compiler error if the interface changes anytime */
     void IAudioUIControllable.SetMasterVolume(float value)
@@ -66,5 +70,14 @@ public class AudioController : MonoBehaviour, IAudioUIControllable
             game.audioMixer.SetFloat("LowPass", 0.3f);
         else
             game.audioMixer.SetFloat("LowPass", 1f);
+    }
+    public void PlayButtonSelect()
+    {
+        Debug.Log("SOUND PLAYED HURRAH");
+        audioSRC.PlayOneShot(onSelect);
+    }
+    public void PlayButtonClick()
+    {
+        audioSRC.PlayOneShot(onClick[0]);
     }
 }
