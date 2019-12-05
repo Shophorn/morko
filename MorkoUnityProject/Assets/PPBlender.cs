a87e2fed67f50995d4aa6b1c81280fc48c7745f3
Iiro <pelttarii@gmail.com>
2019-12-05 13:28:04 +0200

    pp blending

---
+++ MorkoUnityProject/Assets/PPBlender.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PPBlender : MonoBehaviour
+{
+    public PostProcessVolume normal;
+    public PostProcessVolume morko;
+    [Range(0f,1f)]
+    public float blendToMorko = 0;
+
+    void Update()
+    {
+        blendToMorko = Mathf.Clamp01(blendToMorko);
+        normal.weight = 1 - blendToMorko;
+        morko.weight = blendToMorko;
+    }
+}
