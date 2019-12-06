cbf5755f50e888742920fad63261aa9173709c4c
SAMPROO <sampo.tuisku@gmail.com>
2019-12-06 14:33:04 +0200

    Real anims to test

---
+++ MorkoUnityProject/Assets/AnimatorTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorTest : MonoBehaviour
+{
+    public Animator a;
+
+    private bool running = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            a.ResetTrigger("Run");
+            a.ResetTrigger("Idle");
+            a.SetTrigger("Walk");
+        }
+        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            a.ResetTrigger("Walk");
+            a.ResetTrigger("Idle");
+            a.SetTrigger("Run");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            a.ResetTrigger("Walk");
+            a.ResetTrigger("Run");
+            a.ResetTrigger("Idle");
+            a.SetTrigger("Roar");
+        }
+        
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            a.ResetTrigger("Walk");
+            a.ResetTrigger("Run");
+            a.SetTrigger("Idle");
+        }
+    }
+}
