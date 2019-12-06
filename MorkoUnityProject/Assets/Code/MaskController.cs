7b12b994017ce4736ea7e359a927d398275e1a95
SAMPROO <sampo.tuisku@gmail.com>
2019-12-06 04:39:35 +0200

    Navmesh agent offset fix

--- MorkoUnityProject/Assets/Code/MaskController.cs
+++ MorkoUnityProject/Assets/Code/MaskController.cs
@@ -4,7 +4,6 @@ using UnityEngine.AI;
 
 public class MaskController : MonoBehaviour
 {
-    public Transform mask;
     public NavMeshAgent navMeshAgent;
     // Note(Sampo): Leo character lista saadaan varmaan GameManagerista
     public Transform[] characters;
@@ -38,13 +37,12 @@ public class MaskController : MonoBehaviour
     public Transform p1;
     public Transform p2;
     public bool startMaskControllerInStartMethod = true;
-    public Vector3 jumpToOffset = Vector3.zero;
 
     private void Start()
     {
-        navMeshAgent = mask.GetComponent<NavMeshAgent>();
+        navMeshAgent = transform.GetComponent<NavMeshAgent>();
         navMeshAgent.Warp(transform.position);
-        animator = mask.GetComponent<Animator>();
+        animator = transform.GetComponent<Animator>();
 
         if (startMaskControllerInStartMethod)
             this.InvokeAfter (FindStartingCharacter, secondsBeforeMaskMovesAtStart);
@@ -96,7 +94,7 @@ public class MaskController : MonoBehaviour
         
         foreach (var c in characters)
         {
-            var characterDistance = Vector3.Distance(mask.transform.position, c.position);
+            var characterDistance = Vector3.Distance(transform.position, c.position);
             if (characterDistance < distance)
             {
                 distance = characterDistance;
@@ -109,20 +107,20 @@ public class MaskController : MonoBehaviour
     private void MoveMaskToTarget(Transform target, float speed)
     {
         maskMovingToNewMorko = true;
+        
         animator.applyRootMotion = true;
         animator.ResetTrigger("MaskOff");
         animator.ResetTrigger("MaskOn");
         animator.SetBool("Move", true);
         
+        navMeshAgent.enabled = true;
         navMeshAgent.speed = speed;
         navMeshAgent.destination = target.position;
-
-        maskMovingToNewMorko = true;
     }
 
     private void CheckMaskDistanceFromCharacter(Transform target)
     {
-        var distance = Vector3.Distance(mask.transform.position, target.position);
+        var distance = Vector3.Distance(transform.position, target.position);
         if (distance <= minDistanceFromCharacter)
             TransitionMaskToHead(target);
     }
@@ -141,32 +139,28 @@ public class MaskController : MonoBehaviour
     public void MaskOffHead(Transform fromMorko, Transform toMorko)
     {
         animator.SetTrigger("MaskOff");
-        navMeshAgent.Resume(); 
-        navMeshAgent.baseOffset = 0;
-        mask.transform.parent = null;
+        transform.parent = null;
         
-        var direction = (toMorko.position - mask.position).normalized;
-        mask.rotation = Quaternion.LookRotation(direction);
+        var direction = (toMorko.position - transform.position).normalized;
+        transform.rotation = Quaternion.LookRotation(direction);
     }
 
     public void TransitionMaskToHead(Transform toMorko)
     {
+        navMeshAgent.enabled = false;
+
         if (!maskJumping)
         {
             startJumpingPosition = transform.localPosition;
             animator.ResetTrigger("Move");
-            animator.SetTrigger("MaskJumpToHead");
-            navMeshAgent.Stop();
         }
         
-        Vector3 toMorkoHeadPosition = toMorko.transform.GetChild(0).position + jumpToOffset;
+        Vector3 toMorkoHeadPosition = toMorko.transform.GetChild(0).position;
         
-        float lenghtToTarget = Vector3.Distance(startJumpingPosition, toMorkoHeadPosition);
+        float lengthToTarget = Vector3.Distance(startJumpingPosition, toMorkoHeadPosition);
         float currentLengthToTarget = Vector3.Distance(transform.position, toMorkoHeadPosition);
-        float interpolation = (lenghtToTarget - currentLengthToTarget) / lenghtToTarget;
+        float interpolation = (lengthToTarget - currentLengthToTarget) / lengthToTarget;
 
-        //transform.position = Vector3.Lerp(startJumpingPosition, toMorkoHeadPosition, Time.deltaTime * jumpSpeed);
-        navMeshAgent.enabled = false;
         transform.position = Vector3.MoveTowards(transform.position, toMorkoHeadPosition, Time.deltaTime * jumpSpeed);
         animator.Play("MaskJumpToHeadProto", 0, interpolation);
         
@@ -176,33 +170,33 @@ public class MaskController : MonoBehaviour
         {
             maskJumping = false;
             maskIsBeingPutOn = true;
-            navMeshAgent.enabled = true;
-            animator.ResetTrigger("MaskJumpToHead");
             animator.Play("MaskOn");
         }
     }
     
     public void MaskOffMorko()
     {
+        transform.parent = null;
+        var direction = (nextMorko.position - transform.position).normalized;
+        transform.rotation = Quaternion.LookRotation(direction);
+        
+        navMeshAgent.enabled = true;
         navMeshAgent.Resume();
-        navMeshAgent.baseOffset = 0f;
-        mask.transform.parent = null;
-        var direction = (nextMorko.position - mask.position).normalized;
-        mask.rotation = Quaternion.LookRotation(direction);
     }
     public void MaskOnNewMorko()
     {
+        navMeshAgent.enabled = false;
+
         lookingForStartingMorko = false;
         maskMovingToNewMorko = false;
         maskJumping = false;
 
         animator.applyRootMotion = false;
-        
+
         var maskHolder = nextMorko.transform.GetChild(0);
-        mask.transform.parent = maskHolder.transform;
-        mask.transform.localPosition = Vector3.zero;
-        mask.transform.forward = nextMorko.forward;
-        navMeshAgent.baseOffset = 1.16f;
+        transform.parent = maskHolder.transform;
+        transform.localPosition = Vector3.zero;
+        transform.forward = nextMorko.forward;
 
         currentMorko = nextMorko;
     }
