f9b34ee25fa5f2916c97a14f9ccb6a6cbb73d0ee
SAMPROO <sampo.tuisku@gmail.com>
2019-12-10 00:33:24 +0200

    Navmesh and mask blink

---
+++ MorkoUnityProject/Assets/MaskEyeBlink.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MaskEyeBlink : MonoBehaviour
+{
+    private Renderer renderer;
+    private Material material;
+    private Color color;
+    public float blinkTimer = 3f;
+    public float blindDuration = 0.4f;
+    public float offset;
+    
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        material = renderer.material;
+        color = material.GetColor("_EmissionColor");
+    }
+
+    void Update()
+    {
+        float time = Mathf.PingPong(Time.time, blinkTimer);
+        if (time > blindDuration + offset)
+            material.SetColor("_EmissionColor", color);
+        else
+            material.SetColor("_EmissionColor", color * 0f);
+    }
+}
